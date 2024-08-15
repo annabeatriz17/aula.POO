6117140bf2f2b21fd6a7249d631aa3524ad45e58
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-08-15 09:06:00 -0300

    first commit

---
+++ atividade.js
@@ -0,0 +1,54 @@
+const livro = new Object();
+
+livro.titulo = "A Biblioteca da Meia-Noite";
+livro.autor = "Matt Haig";
+livro.ano = 2020;
+
+console.log(livro);
+livro.ano = 2021;
+console.log(livro);
+
+
+const aluno = new Object();
+
+aluno.nome = "Lívia";
+aluno.turma = "segundo ano do primário"
+aluno.ano = 2024
+
+console.log(aluno);
+
+
+const carro = new Object();
+
+carro.cor = "vermelho";
+carro.modelo = "onix"
+carro.ano = 2018;
+
+console.log(carro);
+
+
+const filme = new Object();
+
+filme.titulo = "Gente Grande"
+filme.diretor = "Dennis Dugan";
+filme.anoLançamento = 2010;
+
+console.log(filme);
+
+
+const cachorro = new Object();
+
+cachorro.nome = "Mel";
+cachorro.raça = "Buldogue francês";
+cachorro.cor = "marrom";
+
+console.log(cachorro);
+
+
+const produto = new Object();
+
+produto.descrição = "Batom liquido velvety";
+produto.marca = "Ruby Rose"
+produto.preço = 22.28
+
+console.log(produto);
